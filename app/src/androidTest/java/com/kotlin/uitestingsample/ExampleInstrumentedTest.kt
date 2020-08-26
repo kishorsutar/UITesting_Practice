1a9a013cc809b3968af8e10bb3499138832e1437
Chaitra <chaitra.manjunatha@tigerspike.com>
2020-08-26 17:32:03 +1000

    First instrumentation test case.

--- app/src/androidTest/java/com/kotlin/uitestingsample/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/kotlin/uitestingsample/ExampleInstrumentedTest.kt
@@ -1,12 +1,20 @@
 package com.kotlin.uitestingsample
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +23,28 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    var activityRule: ActivityTestRule<MainActivity>
+            = ActivityTestRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.kotlin.uitestingsample", appContext.packageName)
+        assertEquals("com.example.myapplication", appContext.packageName)
     }
+
+    @Test
+    fun verifyText(){
+        onView(withId(R.id.helloWorld)).check(matches(withText("Hello World!")))
+    }
+
+    @Test
+    fun verifyChangedText(){
+        onView(withId(R.id.edit_text_t)).perform(typeText("testttttttgtygfutygutgtgtgtygtgyguguygugugut"), closeSoftKeyboard())
+        onView(withId(R.id.image)).perform(click())
+        onView(withId(R.id.app_hello_text)).check(matches(withText("testttttttgtygfutygutgtgtgtygtgyguguygugugut")))
+    }
+
 }
--- app/src/main/java/com/kotlin/uitestingsample/MainActivity.kt
+++ app/src/main/java/com/kotlin/uitestingsample/MainActivity.kt
@@ -2,10 +2,15 @@ package com.kotlin.uitestingsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        image.setOnClickListener {
+            app_hello_text.text = edit_text_t.text
+        }
     }
 }
