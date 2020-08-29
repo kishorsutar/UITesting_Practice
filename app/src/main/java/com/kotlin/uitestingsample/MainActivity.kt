be480e8f09cfc394578e9a90f8486d5f2013a781
Kishor Sutar <kishor.sutar@tigerspike.com>
2020-08-29 20:57:04 +0100

    Added Second activity navigation

--- app/src/main/java/com/kotlin/uitestingsample/MainActivity.kt
+++ app/src/main/java/com/kotlin/uitestingsample/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.kotlin.uitestingsample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            startActivity(Intent(this, SecondActivity::class.java))
+        }
     }
 }
